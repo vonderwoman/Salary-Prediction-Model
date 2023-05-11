dd94b935544ba19929edd9f5694eefb6f6c0e22f
Sulekha mohamud <sulekhasabah@gmail.com>
2023-05-11 21:31:23 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from predict_page import show_predict_page
+from explore_page import show_explore_page
+
+#Add a sidebar
+page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+
+    show_explore_page()
